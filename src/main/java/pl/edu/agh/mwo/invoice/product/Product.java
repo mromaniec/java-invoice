7efbdc1c72ed70eda075fef8ae1f0cc39cf0eea7
Magdalena Romaniec <maromaniec@student.agh.edu.pl>
2023-02-11 15:23:25 +0100

    All tests of ProductTest working

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
+//   git token:   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
 
 public abstract class Product {
     private final String name;
@@ -10,6 +11,14 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (name == null || name.isEmpty() ){
+            throw new IllegalArgumentException("Product name cannot be null");
+        }
+
+        if (price == null || price.compareTo(BigDecimal.ZERO) == -1) {
+            throw new IllegalArgumentException("Product price cannot be null or negative");
+        }
+
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
@@ -26,4 +35,5 @@ public abstract class Product {
     public BigDecimal getTaxPercent() {return this.taxPercent;}
 
     public BigDecimal getPriceWithTax() {return this.price.multiply(this.taxPercent).add(this.price);}
+
 }
